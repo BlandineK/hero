3ef8d4ac3dae4c1d6a5d26b3b518a173f0883c2e
BlandineK <blandineuwase222@gmail.com>
2019-09-23 10:38:39 +0200

    import ModelAndView

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Map;
-//import java.util.HashMap;
-import spark.ModelAndView;
+import java.util.HashMap;
+//import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
